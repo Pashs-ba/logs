34ed7f39d40a44af24d7f55e992210086559ba0c
Pashs-ba <pashs.ba@gmail.com>
2019-10-05 09:46:20 +0300

    Add files via upload
    add custom keys

--- log.py
+++ log.py
@@ -2,8 +2,14 @@ import time
 import json
 import os.path
 
-def save(row_data, name = 'log'):
-	data = {time.strftime('%d %b %Y %H:%M:%S', time.localtime()): row_data}
+__author__ = 'Pashs.ba'
+
+def save(row_data, name = 'log', custom_name = None):
+	if custom_name is None:
+		key = time.strftime('%d %b %Y %H:%M:%S', time.localtime())
+	else:
+		key = custom_name
+	data = {key: row_data}
 	if os.path.isfile(name+'.json'):
 		with open(name+'.json', mode='r') as file:
 			a: dict = file.read()
@@ -21,6 +27,6 @@ def load(name='log'):
 			a: dict = file.read()
 		return json.loads(a)
 
-if __main__=='__name__':
-	save({'GPS_1':True, 'tmp_2':None})
+if __name__=='__main__': # It is test you can remove it
+	save({'GPS_1':True, 'tmp_2':None}, custom_name='qqqq')
 	print(load())
